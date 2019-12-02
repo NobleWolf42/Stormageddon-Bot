5f76585dd1305a60881eccfa72d38df02944508d
CzRSpecV <czrspecv.dev@gmail.com>
2019-12-01 22:39:14 -0700

    Added Chat Clear

--- storm.js
+++ storm.js
@@ -265,8 +265,50 @@ client.on("message", message => {
         }, 5000, 7)
     }
     //#endregion
-});
 
+    //#region Chat Clear
+    if((command === (prefix + 'clear')) && (adminTF == true)) {
+        var amount = userInput[1];
+        var passed = true;
+
+        if(isNaN(amount)) {
+            const embMsg = new Discord.RichEmbed()
+                .setTitle('Error!')
+                .setColor(0xb50000)
+                .setDescription('That is not a valid number for the ' + prefix + 'clear command!');
+            message.channel.send(embMsg);
+            passed = false;
+        } else if(amount < 2 || amount > 100) {
+            const embMsg = new Discord.RichEmbed()
+                .setTitle('Error!')
+                .setColor(0xb50000)
+                .setDescription(userInput[1] + ' is invalid! Number must be between 2 and 100!');
+            message.channel.send(embMsg);
+            passed = false;
+        }
+
+        if(amount >= 2 && amount <= 100) {
+            message.channel.bulkDelete(amount, true).catch(err => {
+                console.error(err);
+                const embMsg = new Discord.RichEmbed()
+                    .setTitle('Error!')
+                    .setColor(0xb50000)
+                    .setDescription('An error occurred while attempting to delete!');
+                message.channel.send(embMsg);
+                passed = false;
+            });
+            if(passed == true) {
+                const embMsg = new Discord.RichEmbed()
+                    .setTitle('Success!')
+                    .setColor(32768)
+                    .setDescription('As Per ' + message.author.tag + ', successfully deleted ' + amount + ' messages!');
+                message.channel.send(embMsg);
+            }
+        }
+    }
+    //#endregion
+    
+});
 //#endregion
 
 //#region Prefix Changing
