7f26623a236d5e491a875b1c6a0265382a371445
NobleWolf42 <greenboyb1@gmail.com>
2019-12-02 13:39:07 -0500

    changed port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const url = require('url');
 const fetch = require('node-fetch');
 const FormData = require('form-data');
 
-const port = 53134;
+const port = 3000;
 
 http.createServer((req, res) => {
 	let responseCode = 404;
