3c942d15d7b5f5aa019f04ab905114ed76c3b578
NobleWolf42 <greenboyb1@gmail.com>
2019-12-04 14:19:30 -0500

    added time check

---
+++ timecheck.js
@@ -0,0 +1,3 @@
+var currentdate = new Date();
+var datetime = currentdate.getDay() + "/" + currentdate.getMonth() + "/" + currentdate.getFullYear() + " @ " + currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
+console.log(datetime);
