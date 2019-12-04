36efe4354ba4b4190eff22753453320a49aa5e2f
NobleWolf42 <greenboyb1@gmail.com>
2019-12-04 14:26:41 -0500

    update time check

--- timecheck.js
+++ timecheck.js
@@ -1,3 +1,4 @@
 var currentdate = new Date();
-var datetime = currentdate.getDay() + "/" + currentdate.getMonth() + "/" + currentdate.getFullYear() + " @ " + currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
+var date = ['January', 'Feburary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var datetime = date[currentdate.getMonth()] + ' ' + currentdate.getDay() + " " + currentdate.getFullYear() + " @ " + currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
 console.log(datetime);
