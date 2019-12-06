e00802d699a38ca89f5f7b6e3c31b46c1624b128
NobleWolf42 <greenboyb1@gmail.com>
2019-12-06 18:47:40 -0500

    continued errorchecking

--- helpers/userHandling.js
+++ helpers/userHandling.js
@@ -42,7 +42,8 @@ function serverRoleUpdate(sRole, serverid) {
 
     //Loops throught the DJ Role Names, pushing them to an array
     for (key in config[serverid].music.djRoles) {
-        
+        console.log(key);
+        console.log(basicServerRoles[config[serverid].music.djRoles[key]]);
         //Pushes role IDs to DJs if they Match config[serverid].music.djRoles
         if (basicServerRoles[config[serverid].music.djRoles[key]]){
             djRoleIDs.push(basicServerRoles[config[serverid].music.djRoles[key]]);
