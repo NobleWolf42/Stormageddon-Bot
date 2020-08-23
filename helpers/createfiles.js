2b3e5db9b7c27fa2ba3e587b006d63047ff105e3
NobleWolf42 <greenboyb1@gmail.com>
2020-08-23 12:47:43 -0400

    change to errorlog to make it dm us on fatal error

--- helpers/createfiles.js
+++ helpers/createfiles.js
@@ -7,22 +7,24 @@ function createJSONfiles() {
 
     var emptyfile = {};
     var d = new Date();
-    var emptyLog = { "logging": [{
-        "Log": "Rebuilt Log File",
-        "Date": d,
-        "Code": "None"
-    }]};
+    var emptyLog = {
+        "logging": [{
+            "Log": "Rebuilt Log File",
+            "Date": d,
+            "Code": "None"
+        }]
+    };
 
     if (!existsSync("./data/botprefix.json")) {
-        writeFileSync("./data/botprefix.json", JSON.stringify(emptyfile), function(err) {
+        writeFileSync("./data/botprefix.json", JSON.stringify(emptyfile), function (err) {
             if (err) {
                 console.log(err);
             }
         });
     }
-    
+
     if (!existsSync("./data/serverconfig.json")) {
-        writeFileSync("./data/serverconfig.json", JSON.stringify(emptyfile), function(err) {
+        writeFileSync("./data/serverconfig.json", JSON.stringify(emptyfile), function (err) {
             if (err) {
                 console.log(err);
             }
@@ -30,7 +32,7 @@ function createJSONfiles() {
     }
 
     if (!existsSync("./data/errorlog.json")) {
-        writeFileSync("./data/errorlog.json", JSON.stringify(emptyLog), function(err) {
+        writeFileSync("./data/errorlog.json", JSON.stringify(emptyLog), function (err) {
             if (err) {
                 console.log(err);
             }
