6e9c4adfdce4c08bfc7474fe3c2a09d2000e8a0e
Charles Bartlett <ownageunit@gmail.com>
2024-08-27 01:35:08 -0400

    WIP: add voice logging function to bot

--- internal/voiceHandling.js
+++ internal/voiceHandling.js
@@ -1,14 +1,20 @@
 //#region Dependencies
-const { Collection, VoiceState, ChannelType, PermissionFlagsBits } = require('discord.js');
-const { readFileSync } = require('fs');
+const {
+  Collection,
+  VoiceState,
+  ChannelType,
+  PermissionFlagsBits,
+  EmbedBuilder,
+} = require("discord.js");
+const { readFileSync } = require("fs");
 //#endregion
 
 //#region Data Files
-var serverConfig = JSON.parse(readFileSync('./data/serverConfig.json', 'utf8'));
+var serverConfig = JSON.parse(readFileSync("./data/serverConfig.json", "utf8"));
 //#endregion
 
 //#region Helpers
-const { addToLog } = require('../helpers/errorLog.js');
+const { addToLog } = require("../helpers/errorLog.js");
 //#endregion
 
 //#region Function that starts the listener that handles Join to Create Channels
@@ -17,72 +23,170 @@ const { addToLog } = require('../helpers/errorLog.js');
  * @param {Client} client - Discord.js Client Object
  */
 async function joinToCreateHandling(client) {
-    client.voiceGenerator = new Collection();
+  client.voiceGenerator = new Collection();
 
-    //This handles the event of a user joining or disconnecting from a voice channel
-    client.on("voiceStateUpdate", async (oldState, newState) => {
-        const { member, guild } = newState;
-        const serverID = guild.id;
-        const oldChannel = oldState.channel;
-        const newChannel = newState.channel;
+  //This handles the event of a user joining or disconnecting from a voice channel
+  client.on("voiceStateUpdate", async (oldState, newState) => {
+    const { member, guild } = newState;
+    const serverID = guild.id;
+    const oldChannel = oldState.channel;
+    const newChannel = newState.channel;
 
-        if (serverConfig[serverID].setupNeeded) {
-            return;
-        }
+    if (serverConfig[serverID].setupNeeded) {
+      return;
+    }
 
-        if ( serverConfig[serverID].JTCVC.enable && oldChannel !== newChannel && newChannel && newChannel.id === serverConfig[serverID].JTCVC.voiceChannel) {
-            //Creates new voice channel
-            const voiceChannel = await guild.channels.create({
-                name: `${member.user.tag}'s Channel`,
-                type: ChannelType.GuildVoice,
-                parent: newChannel.parent,
-                permissionOverwrites: newChannel.parent.permissionOverwrites.cache.map((p) => {
-                    return {
-                        id: p.id,
-                        allow: p.allow.toArray(),
-                        deny: p.deny.toArray()
-                    }
-                })
-            });
+    if (
+      serverConfig[serverID].JTCVC.enable &&
+      oldChannel !== newChannel &&
+      newChannel &&
+      newChannel.id === serverConfig[serverID].JTCVC.voiceChannel
+    ) {
+      //Creates new voice channel
+      const voiceChannel = await guild.channels.create({
+        name: `${member.user.tag}'s Channel`,
+        type: ChannelType.GuildVoice,
+        parent: newChannel.parent,
+        permissionOverwrites: newChannel.parent.permissionOverwrites.cache.map(
+          (p) => {
+            return {
+              id: p.id,
+              allow: p.allow.toArray(),
+              deny: p.deny.toArray(),
+            };
+          }
+        ),
+      });
 
-            //Adds the voice channel just made to the collection
-            client.voiceGenerator.set(voiceChannel.id, member.id);
-            client.voiceGenerator.set(member.id, voiceChannel.id);
-            
-            //Times the user out from spamming new voice channels, currently set to 10 seconds and apparently works intermittently, probably dur to the permissions when testing it
-            await newChannel.permissionOverwrites.edit(member, {deny: PermissionFlagsBits.Connect});
-            setTimeout(() => newChannel.permissionOverwrites.delete(member), 10 * 1000);
+      //Adds the voice channel just made to the collection
+      client.voiceGenerator.set(voiceChannel.id, member.id);
+      client.voiceGenerator.set(member.id, voiceChannel.id);
 
-            return member.voice.setChannel(voiceChannel);
-        }
+      //Times the user out from spamming new voice channels, currently set to 10 seconds and apparently works intermittently, probably dur to the permissions when testing it
+      await newChannel.permissionOverwrites.edit(member, {
+        deny: PermissionFlagsBits.Connect,
+      });
+      setTimeout(
+        () => newChannel.permissionOverwrites.delete(member),
+        10 * 1000
+      );
 
-        //Handles someone leaving a voice channel
-        try {
-            if (oldChannel == null) {
-                return;
-            }
-            if (oldChannel != null && client.voiceGenerator.get(oldChannel.id) && oldChannel.members.size == 0) {
-                //This deletes a channel if it was created byt the bot and is empty
-                oldChannel.delete();
-                client.voiceGenerator.delete(client.voiceGenerator.get(oldChannel.id));
-                client.voiceGenerator.delete(oldChannel.id);
-            } else if (client.voiceGenerator.get(oldChannel.id) && member.id == client.voiceGenerator.get(oldChannel.id)) {
-                //This should restore default permissions to the channel when the owner leaves, and remove owner THIS IS BROKEN AND SERVERS NO PURPOSE RIGHT NOW
-                /*await oldChannel.permissionOverwrites.edit(oldChannel.parent.permissionOverwrites.cache.map((p) => {
+      return member.voice.setChannel(voiceChannel);
+    }
+
+    //Handles someone leaving a voice channel
+    try {
+      if (oldChannel == null) {
+        return;
+      }
+      if (
+        oldChannel != null &&
+        client.voiceGenerator.get(oldChannel.id) &&
+        oldChannel.members.size == 0
+      ) {
+        //This deletes a channel if it was created byt the bot and is empty
+        oldChannel.delete();
+        client.voiceGenerator.delete(client.voiceGenerator.get(oldChannel.id));
+        client.voiceGenerator.delete(oldChannel.id);
+      } else if (
+        client.voiceGenerator.get(oldChannel.id) &&
+        member.id == client.voiceGenerator.get(oldChannel.id)
+      ) {
+        //This should restore default permissions to the channel when the owner leaves, and remove owner THIS IS BROKEN AND SERVERS NO PURPOSE RIGHT NOW
+        /*await oldChannel.permissionOverwrites.edit(oldChannel.parent.permissionOverwrites.cache.map((p) => {
                     return {
                         id: p.id,
                         allow: p.allow.toArray(),
                         deny: p.deny.toArray()
                     }
                 }));*/
-                client.voiceGenerator.delete(client.voiceGenerator.get(oldChannel.id));
-            }
-        } catch (err) {
-            addToLog('Fatal Error', "JTCVC Handler", member.tag, guild.name, oldChannel.name, err, client);
-        }
-
-    });
+        client.voiceGenerator.delete(client.voiceGenerator.get(oldChannel.id));
+      }
+    } catch (err) {
+      addToLog(
+        "Fatal Error",
+        "JTCVC Handler",
+        member.tag,
+        guild.name,
+        oldChannel.name,
+        err,
+        client
+      );
+    }
+  });
 }
+//#endregion
+
+//#region
+/**
+ *
+ * @param {*} client
+ */
+async function logOnVoiceStateUpdate(client) {
+  // setup a listener for voice state update
+  client.on("voiceStateUpdate", async (oldState, newState) => {
+    // extract the member? and the channel
+    const { member, guild } = newState;
+    const serverID = guild.id;
+    const newChannel = newState.channel;
+    const oldChannel = oldState.channel;
+
+    // check the server config and see if they have logging turned on
+    // is the bot setup on the server?
+    if (serverConfig[serverID].setupNeeded) {
+      return;
+    }
+
+    // is logging turned on?
+    if (!serverConfig[serverID].logging.voiceConnect) {
+      return;
+    }
+
+    // is logging channel defined?
+    if (serverConfig[serverID].logging.loggingChannel == "Not Set Up") {
+      return addToLog(
+        "Fatal Error",
+        "logOnVoiceStateUpdate",
+        "null",
+        guild.name,
+        "null",
+        "The logging output channel is not setup.",
+        client
+      );
+    }
+
+    // any time voice state updated execute the following code
+    if (oldChannel == null || oldChannel.guild.id != newChannel.guild.id) {
+      const embMsg = new EmbedBuilder()
+        .setTitle(`User ${member.tag} connected!`)
+        .setColor("#ffffff")
+        .setDescription("test we'll figure it out later ben I'm tired")
+        .setTimestamp();
+
+      // Trigger every time a connection or disconnection is made to a channel
+      // wrap in message (embeds)
+      // post message to channel
+      await client.channels
+        .fetch(serverConfig[serverID].logging.loggingChannel)
+        .then((channel) => channel.send({ embeds: [embMsg] }));
+    }
+    if (newChannel == null || newChannel.guild.id != oldChannel.guild.id) {
+      const embMsg = new EmbedBuilder()
+        .setTitle(`User ${member.tag} disconnected!`)
+        .setColor("#ffffff")
+        .setDescription("test we'll figure it out later ben I'm tired")
+        .setTimestamp();
+
+      // Trigger every time a connection or disconnection is made to a channel
+      // wrap in message (embeds)
+      // post message to channel
+      await client.channels
+        .fetch(serverConfig[serverID].logging.loggingChannel)
+        .then((channel) => channel.send({ embeds: [embMsg] }));
+    }
+  });
+}
+
 //#endregion
 
 //#region exports
